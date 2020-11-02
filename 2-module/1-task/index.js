005e3604f76fdfc1a124d25716ed2d8cd2fb28e0
Vlad Svec <mehomeisgood@gmail.com>
2020-11-02 17:16:56 +0200

    3task

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -3,6 +3,17 @@
  * @param {Object} salaries - объект зарплат
  * @returns {Number}
  */
+
 function sumSalary(salaries) {
-  // ваш код...
+  let sumOfSalaries = 0;
+
+  for (let key in salaries) {
+    let isSalary = typeof salaries[key] === 'number';
+
+    if (isSalary) {
+      sumOfSalaries += salaries[key];
+    }
+  }
+
+  return sumOfSalaries;
 }
