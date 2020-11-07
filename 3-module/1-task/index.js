e0ec75631bef14811f137aacbeb7e0fb38562d0e
Vlad Svec <mehomeisgood@gmail.com>
2020-11-07 18:52:24 +0200

    3task

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -2,6 +2,8 @@
  * @param   {{ name: string, age: number }[]} users
  * @returns {string[]}  объект
  */
-function namify(users) {
-  // ваш код...
+
+ function namify(users) {
+  let names = users.map(item => item.name);
+  return names;
 }
--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -5,5 +5,13 @@
  * @returns {number[]}
  */
 function filterRange(arr, a, b) {
-  // ваш код...
+  return arr.filter(item => (a <= item && item <= b));
 }
+
+let arr = [5, 3, 8, 1];
+
+let filtered = filterRange(arr, 1, 4);
+
+alert( filtered ); 
+
+alert( arr ); 
